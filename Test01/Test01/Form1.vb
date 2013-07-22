d71d03d6e0d1acd3a6e5f3b8e896e6bebd54d85a
Ludwig <50410709@mylife.unisa.ac.za>
2013-07-22 20:37:34 +0200

    This is just to test if this will work or not
    Signed-off-by: Ludwig <50410709@mylife.unisa.ac.za>

---
+++ Test01/Test01/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Button1.Text = "Okay"
+    End Sub
+End Class
