fb33ee2d90a84aadfd177860e35596b47310ad67
Ludwig <50410709@mylife.unisa.ac.za>
2013-07-23 22:52:27 +0200

    Changed the label1 name on form to open
    Signed-off-by: Ludwig <50410709@mylife.unisa.ac.za>

--- Test01/Test01/Form1.vb
+++ Test01/Test01/Form1.vb
@@ -1,10 +1,13 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Label1.Text = "This program will now close"
+
         Close()
     End Sub
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Button1.Text = "Okay"
+        Label1.Text = "Open"
     End Sub
 End Class
